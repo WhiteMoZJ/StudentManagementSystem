ea194d5d92b31a96a813d10c57d53421b4bc8750
Jun Chau <whitemozj@outlook.com>
2021-12-31 19:35:19 +0800

    变更加密方式

--- PWDEncryption.py
+++ PWDEncryption.py
@@ -1,37 +1,20 @@
-import asc
-import random
-import math
-
-#生成密钥串（使用RSA加密）
-def Key():
-    p = random.randint(1, 999)
-    q = random.randint(1,999)
-    N = p * q
-    L = math.lcm((p-1),(q-1))
-    for i in range(2,L):
-        if math.gcd(i,L) == 1:
-            E = i
-            break
-    for j in range(2,L):
-        if (E*j)%L == 1:
-            D = j
-            break
-    return E,D,N
-
-#加密密码字符（密钥串，密码字符）
-def Encrypt(E,D,N,a):
-    b = asc.len(a) #字符串长度，用作循环
-    n = [] #加密前的字符串转ASCII
-    ecp = [] #加密后的ASCII组成的列表
-    key = [E,D,N] #密钥串
-    for i in range(b):
-        num = asc.str2asc(i,a)
-        n.append(num)
-        ecp.append((n[i]**key[1])%key[3])
-    ecpwd = (ecp,key)
-    #输出格式[加密后的整数列表]+[密钥串]
-    print(ecpwd)
-
-a = str(input())
-E,D,N = Key()
-Encrypt(E,D,N,a)
+def Encrypt(s):
+    k = 'djq%5cu#-jeq15abg$z9_i#_w=$o88m!*alpbedlbat8cr74sd'
+    encry_str = ""
+    for i,j in zip(s,k):
+    # i为字符，j为秘钥字符
+        temp = str(ord(i)+ord(j))+'_' 
+        # 加密字符 = 字符的Unicode码 + 秘钥的Unicode码
+        encry_str = encry_str + temp
+    return encry_str
+ 
+# 解密
+def Decrypt(p):
+    k = 'djq%5cu#-jeq15abg$z9_i#_w=$o88m!*alpbedlbat8cr74sd'
+    dec_str = ""
+    for i,j in zip(p.split("_")[:-1],k):
+        # i 为加密字符，j为秘钥字符
+        temp = chr(int(i) - ord(j)) 
+        # 解密字符 = (加密Unicode码字符 - 秘钥字符的Unicode码)的单字节字符
+        dec_str = dec_str+temp
+    return dec_str
